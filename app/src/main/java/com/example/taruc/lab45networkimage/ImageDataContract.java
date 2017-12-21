0ef2eec410d70f3e9bcc8b610a370422fabc08a4
xiiaoyumi <yuuki_asunaaaa@hotmail.com>
2017-12-21 11:52:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/lab45networkimage/ImageDataContract.java
@@ -0,0 +1,12 @@
+package com.example.taruc.lab45networkimage;
+
+/**
+ * Created by KweeTeck on 11/1/2017.
+ */
+
+public class ImageDataContract {
+    public static final String TABLE_NAME = "album";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_IMAGE="image";
+}
